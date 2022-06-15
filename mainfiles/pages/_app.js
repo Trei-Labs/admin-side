6aae21f66b270500161f6841fb22009b76cb5801
Arch0125 <dasarchisman25@gmail.com>
2022-06-15 11:24:50 +0530

    Add admin page

--- mainfiles/pages/_app.js
+++ mainfiles/pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
-
+import fonts from '../styles/fonts.css'
+import cards from '../styles/cards.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
