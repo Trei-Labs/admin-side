372595585d5f927e26dc10566180bb19b9cbfc24
Arch0125 <dasarchisman25@gmail.com>
2022-06-16 13:40:12 +0530

    Contract Interaction Complete

---
+++ mainfiles/src/commons/CashBackDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useContract,useSigner } from 'wagmi';
+import GetAccount from '../hooks/GetAccount';
+
+const CashBackDisplay =() =>{
+    return(
+        <div className=' ml-[20px] w-[100%] h-[100px] bg-white ' >
+            Hello
+        </div>
+    )
+}
+
+export default CashBackDisplay
